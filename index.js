0af8e6680bb097fbf6a6b9d225c57580c55db066
econb2004 <38587142+econb2004@users.noreply.github.com>
2022-06-23 10:51:31 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+function print(name, height, country){
+  console.log("Name: " + name)
+  console.log("Height: " + height)
+  console.log("Country: " + country)
+}
+
+print("Henry", 10.2, "Nigeria")
